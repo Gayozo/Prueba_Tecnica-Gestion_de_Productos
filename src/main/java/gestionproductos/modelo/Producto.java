040225c4c07c87e14b8e073cbbf93b18984db920
Samuel Gayozo <albertosamuel.gayozo@gmail.com>
2026-09-21 22:21:45 -0300

    Creación de la clase Producto

---
+++ src/main/java/gestionproductos/modelo/Producto.java
@@ -0,0 +1,107 @@
+package main.java.gestionproductos.modelo;
+
+public class Producto {
+
+    private Integer id;
+    private String codigo;
+    private String nombre;
+    private String categoria;
+    private double precio;
+    private int stock;
+    private String estado;
+
+    // Constructor
+    public Producto() {
+    }
+
+    // Constructor sin ID (para registrar nuevos productos antes de enviarlos a la DB)
+    public Producto(String codigo, String nombre, String categoria, double precio, int stock, String estado) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.stock = stock;
+        this.estado = estado;
+    }
+
+    // Constructor completo (usado al momento de consultar los datos de la DB)
+    public Producto(Integer id, String codigo, String nombre, String categoria, double precio, int stock, String estado) {
+        this.id = id;
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.stock = stock;
+        this.estado = estado;
+    }
+
+    // Getters y Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "id=" + id +
+                ", codigo='" + codigo + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", categoria='" + categoria + '\'' +
+                ", precio=" + precio +
+                ", stock=" + stock +
+                ", estado='" + estado + '\'' +
+                '}';
+    }
+}
